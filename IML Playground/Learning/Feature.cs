0839c53fc0b8e46b84613d238a63c6ee17e4ca83
fflewddur <todd@dropline.net>
2013-10-28 17:24:15 -0700

    Add a Feature class to store user-visible details about each feature.

---
+++ IML Playground/Learning/Feature.cs
@@ -0,0 +1,35 @@
+﻿using IML_Playground.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IML_Playground.Learning
+{
+    [Serializable]
+    class Feature
+    {
+        private string _characters;
+        private int _count;
+        private double _weight;
+
+        public string Characters
+        {
+            get { return _characters; }
+            set { _characters = value; }
+        }
+
+        public int Count
+        {
+            get { return _count; }
+            set { _count = value; }
+        }
+
+        public double Weight
+        {
+            get { return _weight; }
+            set { _weight = value; }
+        }
+    }
+}
