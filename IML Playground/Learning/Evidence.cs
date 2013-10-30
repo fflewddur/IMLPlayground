8651e0dcf5cecd694e7cfc19df91de1c347141e6
fflewddur <todd@dropline.net>
2013-10-29 17:32:24 -0700

    Display evidence underlying each prediction.

--- IML Playground/Learning/Evidence.cs
+++ IML Playground/Learning/Evidence.cs
@@ -10,5 +10,26 @@ namespace IML_Playground.Learning
     {
         public Dictionary<int, double> Weights;
         public double Confidence;
+        private Vocabulary Vocab;
+
+        public Evidence(Vocabulary vocab)
+        {
+            Confidence = -1;
+            Weights = new Dictionary<int, double>();
+            Vocab = vocab;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("Confidence={0:0.000} ", Confidence);
+            foreach (KeyValuePair<int, double> pair in Weights)
+            {
+                sb.AppendFormat("{0}={1:0.000} ", Vocab.GetWord(pair.Key), pair.Value);
+            }
+
+            return sb.ToString();
+        }
     }
 }
--- IML Playground/Learning/Prediction.cs
+++ IML Playground/Learning/Prediction.cs
@@ -9,6 +9,23 @@ namespace IML_Playground.Learning
     class Prediction
     {
         public Label Label;
-        public Dictionary<Label, Evidence> Evidence;
+        public Dictionary<Label, Evidence> EvidencePerClass;
+
+        public Prediction()
+        {
+            EvidencePerClass = new Dictionary<Label, Evidence>();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (KeyValuePair<Label, Evidence> pair in EvidencePerClass)
+            {
+                sb.AppendFormat("{0}: {1}\n", pair.Key.UserLabel, pair.Value);
+            }
+
+            return sb.ToString();
+        }
     }
 }
