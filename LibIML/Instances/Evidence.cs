1b7e00ebd8a1881dab14b97538030ddac069d070
fflewddur <todd@dropline.net>
2014-03-10 11:26:34 -0700

    Cleanup Evidence class.

--- LibIML/Instances/Evidence.cs
+++ LibIML/Instances/Evidence.cs
@@ -10,19 +10,14 @@ namespace LibIML
     public class Evidence
     {
         public List<EvidenceItem> Items;
-        //public Dictionary<int, int> Counts;
-        //public Dictionary<int, double> Weights;
         public double ClassPr;
         public double Confidence;
-        //private Vocabulary Vocab;
 
         public Evidence()
         {
             Confidence = -1;
             ClassPr = -1;
             Items = new List<EvidenceItem>();
-            //Weights = new Dictionary<int, double>();
-            //Vocab = vocab;
         }
 
         #region Override methods
@@ -36,10 +31,6 @@ namespace LibIML
             foreach (EvidenceItem item in Items) {
                 sb.AppendFormat("{0}={1}*({2:N4}+{3:N4}) ", item.Word, item.Count, item.SystemWeight, item.UserWeight);
             }
-            //foreach (KeyValuePair<int, double> pair in Weights)
-            //{
-            //    sb.AppendFormat("{0}={1:N4} ", Vocab.GetWord(pair.Key), pair.Value);
-            //}
 
             return sb.ToString();
         }
