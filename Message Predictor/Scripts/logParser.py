d3439e923c6b8b76e80d38f2110a084d67ba6d4b
Todd Kulesza <todd@dropline.net>
2014-07-11 13:46:33 -0700

    Support parsing BoW and feature F1 averages.

--- Message Predictor/Scripts/logParser.py
+++ Message Predictor/Scripts/logParser.py
@@ -5,9 +5,10 @@
 # Author: Todd Kulesza <todd@dropline.net>
 # Created: June 2014
 
-import sys
-import xml.etree.ElementTree as etree
 import argparse
+import csv
+import os, sys
+import xml.etree.ElementTree as etree
 
 LOG_TYPE_UNKNOWN = 0
 LOG_TYPE_ACTIONS = 1
@@ -47,6 +48,7 @@ def parseActionsLog(root):
     userid = root.attrib["userid"]
     results['condition'] = condition
     results['userid'] = userid
+    results['type'] = 'actions'
 
     # When did we start?
     windowOpen = root.find("WindowOpen")
@@ -121,34 +123,43 @@ def parseActionsLog(root):
 
     return (LOG_TYPE_ACTIONS, results)
 
-def parseEvalSection(root, elementName, results, onlyEvery=5):
+def parseEvalSection(root, elementName, results, bucketOn='order', bucketSize=5):
     evals = root.findall("./" + elementName)
-    scores = []
-    c = 0
+    scores = dict()
+    f1Sum = 0.0
     for evaluation in evals:
-        c += 1
-        if c % onlyEvery == 0:
-            f1w = getF1(evaluation)
-            wl = getWrongLabels(evaluation)
-            o = evaluation.attrib['order']
-            vs = evaluation.attrib['vocabSize']
-            tss = evaluation.attrib['trainingSetSize']
-            scores.append((c, f1w, o, vs, tss, wl))
+        # print("evaluation = {0}".format(evaluation))
+        c = int(evaluation.attrib[bucketOn])
+        f1Sum += getF1(evaluation)
+        if c % bucketSize == 0:
+            scores[c] = (f1Sum / bucketSize)
+            f1Sum = 0.0
+
+        # f1w = getF1(evaluation)
+        # f1Sum += float(f1w.text)
+        # c += 1
+        # if c % bucketSize == 0:
+        #     f1w = getF1(evaluation)
+        #     wl = getWrongLabels(evaluation)
+        #     o = evaluation.attrib['order']
+        #     vs = evaluation.attrib['vocabSize']
+        #     tss = evaluation.attrib['trainingSetSize']
+        #     scores.append((c, f1w, o, vs, tss, wl))
     results[elementName] = scores
 
-def parseEvaluationsLog(root):
+def parseEvaluationsLog(root, bucketSize=5):
     results = dict()
 
     condition = root.attrib["condition"]
     userid = root.attrib["userid"]
     results['condition'] = condition
     results['userid'] = userid
+    results['type'] = 'evaluations'
 
     evaluations = root.find("Evaluations")
 
     # Look at the classifier's accuracy over time
     evals = evaluations.findall("./Evaluation")
-    bucketSize = 5 # bin in 5-minute intervals
     c = 0
     f1Sum = 0.0
     for evaluation in evals:
@@ -160,15 +171,15 @@ def parseEvaluationsLog(root):
             results[key] = f1Sum / bucketSize
             f1Sum = 0.0;
 
-    parseEvalSection(evaluations, "featuresAdded", results, 2)
-    parseEvalSection(evaluations, "featuresAddedBoW", results, 2)
-    parseEvalSection(evaluations, "messagesLabeled", results, 2)
-    parseEvalSection(evaluations, "messagesLabeledBoW", results, 2)
+    parseEvalSection(evaluations, "featuresAdded", results, bucketSize=bucketSize)
+    parseEvalSection(evaluations, "featuresAddedBoW", results, bucketSize=bucketSize)
+    parseEvalSection(evaluations, "messagesLabeled", results, bucketSize=bucketSize)
+    parseEvalSection(evaluations, "messagesLabeledBoW", results, bucketSize=bucketSize)
 
     return (LOG_TYPE_EVALUATIONS, results)
 
-def parseLogfile(logfile):
-    print("Parsing log '{0}'".format(logfile))
+def parseLogfile(logfile, bucketSize=5):
+    print("[INFO] Parsing log '{0}'".format(logfile))
 
     tree = etree.parse(logfile)
     root = tree.getroot()
@@ -177,7 +188,7 @@ def parseLogfile(logfile):
     if lt == LOG_TYPE_ACTIONS:
         return parseActionsLog(root)
     elif lt == LOG_TYPE_EVALUATIONS:
-        return parseEvaluationsLog(root)
+        return parseEvaluationsLog(root, bucketSize=bucketSize)
     else:
         print("Error: unknown log file type.")
         return (LOG_TYPE_UNKNOWN, None)
@@ -209,23 +220,59 @@ def displayLogResults(lt, results):
         print("Average F1 from minutes 15-19: {0:.3f}".format(results['f1Avg20']))
         print("Average F1 from minutes 20-24: {0:.3f}".format(results['f1Avg25']))
         print("Average F1 from minutes 25-29: {0:.3f}".format(results['f1Avg30']))
-        for score in results['messagesLabeled']:
-            (c, f1w, o, vs, tss, wl) = score
-            print("F1 after {0} messages labeled: {1:.3f} "\
-                  "({2} labels total, {3} incorrect)".format(c, f1w, tss, wl))
-        for score in results['messagesLabeledBoW']:
-            (c, f1w, o, vs, tss, wl) = score
-            print("BoW F1 after {0} messages labeled: {1:.3f} "\
-                  "({2} labels total, {3} incorrect)".format(c, f1w, tss, wl))
-        for score in results['featuresAdded']:
-            (c, f1w, o, vs, tss, wl) = score
-            print("F1 after {0} features added: {1:.3f} "\
-                  "({2} labels total, {3} incorrect)".format(c, f1w, tss, wl))
-        for score in results['featuresAddedBoW']:
-            (c, f1w, o, vs, tss, wl) = score
-            print("BoW F1 after {0} features added: {1:.3f} "\
-                  "({2} labels total, {3} incorrect)".format(c, f1w, tss, wl))
-        # print("results: {0}".format(results))
+        printResultList(results, 'messagesLabeled', 'messages labeled')
+        printResultList(results, 'messagesLabeledBoW', 'messages labeled (BoW)')
+        printResultList(results, 'featuresAdded', 'features added')
+        printResultList(results, 'featuresAddedBoW', 'features added (BoW)')
+
+def printResultList(results, key, desc):
+    for score in results[key]:
+        (c, f1w) = score
+        print("Average F1 after {0} {1}: {2:.3f}".format(c, desc, f1w))
+
+def parseLogdir(d):
+    allResults = dict()
+    for root, dirs, files in os.walk(d):
+        for file in files:
+            if not file.endswith('.xml'):
+                print("[INFO] Ignoring non-XML file ({0})".format(file))
+                continue
+            file = os.path.join(d, file)
+            (logType, fileResults) = parseLogfile(file, bucketSize=10)
+            uid = fileResults['userid']
+            if not allResults.get(uid):
+                allResults[uid] = dict()
+            allResults[uid][fileResults['type']] = fileResults
+
+    print("ID, Treatment, F1Train, F1Test, F1TrainBoW, F1TestBoW, MessagesLabeled, "\
+          "MessagesLabeledPoorly, FeaturesAdded, FeaturesRemoved, FeaturesAdjusted, "\
+          "f1BoW10, f1BoW20, f1BoW30, f1BoW40, f1BoW50, f1BoW60, f1BoW70, f1BoW80, "\
+          "f1FA10, f1FA20, f1FA30, f1FA40, f1FA50, f1FA60, f1FA70, f1FA80")
+    for k in sorted(allResults.keys()):
+        r = allResults[k]['actions']
+        print("{0}, {1}, {2:.3f}, {3:.3f}, {4:.3f}, {5:.3f}, {6}, {7}, {8}, {9}, {10}, ".format(
+            r['userid'], r['condition'], r['f1FinalTraining'], r['f1FinalTest'],
+            r['f1FinalTrainingBoW'], r['f1FinalTestBow'],
+            r['messagesLabeled'], r['messagesLabeledWrong'], r['featuresAdded'],
+            r['featuresRemoved'], r['featuresAdjusted']
+        ), end='')
+        # Get the average F1 scores for various training set sizes with BoW
+        r = allResults[k]['evaluations']['messagesLabeledBoW']
+        for i in range(10, 81, 10):
+            f1 = r.get(i)
+            if f1:
+                print("{0:.3f}, ".format(f1), end='')
+            else:
+                print(", ", end='')
+        # Get the average F1 scores for various number of features
+        r = allResults[k]['evaluations']['featuresAdded']
+        for i in range(10, 81, 10):
+            f1 = r.get(i)
+            if f1:
+                print("{0:.3f}, ".format(f1), end='')
+            else:
+                print(", ", end='')
+        print()
 
 def main():
     ap = argparse.ArgumentParser()
@@ -240,7 +287,8 @@ def main():
     if args.file:
         (lt, results) = parseLogfile(args.file)
         displayLogResults(lt, results)
-
+    if args.dir:
+        parseLogdir(args.dir)
 
 if __name__ == "__main__":
     main()
